4d601a8347c2031f94ca8604e2f1154393d43f3c
Marcio Botelho Junior <marciob@cpqd.com.br>
2020-12-09 00:31:21 -0300

    Estruturação

--- oumeajuda/src/App.js
+++ oumeajuda/src/App.js
@@ -1,45 +1,36 @@
 import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
-import ListaDeCategorias from "./components/ListaDeCategorias/index.js";
+import ListaDeCategorias from "./components/ListaDeCategorias";
 import "./assets/index.css";
 import "./assets/App.css";
+import ArrayDeNotas from "./data/Notas";
+import Categorias from "./data/Categorias";
 
 class App extends Component {
   constructor() {
     super();
-
-    this.notas = [];
-    this.state = {
-      notas: [],
-      categorias: [],
-    };
-  }
-
-  criarNota(titulo, texto) {
-    const novaNota = { titulo, texto };
-    const novoArrayNotas = [...this.state.notas, novaNota];
-    const novoEstado = {
-      notas: novoArrayNotas,
-    };
-    this.setState(novoEstado);
-  }
-
-  deletarNota(index) {
-    let arrayNotas = this.state.notas;
-    arrayNotas.splice(index, 1);
-    this.setState({ notas: arrayNotas });
+    this.categorias = new Categorias();
+    this.notas = new ArrayDeNotas();
   }
 
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <FormularioCadastro
+          categorias={this.categorias}
+          criarNota={this.notas.adicionarNota.bind(this.notas)}
+        />
         <main className="conteudo-principal">
-          <ListaDeCategorias />
+          <ListaDeCategorias
+            adicionarCategoria={this.categorias.adicionarCategoria.bind(
+              this.categorias
+            )}
+            categorias={this.categorias}
+          />
           <ListaDeNotas
-            apagarNota={this.deletarNota.bind(this)}
-            notas={this.state.notas}
+            apagarNota={this.notas.apagarNotas.bind(this.notas)}
+            notas={this.notas}
           />
         </main>
       </section>
--- oumeajuda/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ oumeajuda/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useImperativeHandle } from "react";
+import React, { Component } from "react";
 import "./estilo.css";
 
 class FormularioCadastro extends Component {
@@ -6,6 +6,26 @@ class FormularioCadastro extends Component {
     super(props);
     this.titulo = "";
     this.texto = "";
+    this.categoria = "Sem Categoria";
+    this.state = { categorias: [] };
+    this._novasCategorias = this._novasCategorias.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.categorias.inscrever(this._novasCategorias);
+  }
+
+  componentWillUnmount() {
+    this.props.categorias.desinscrever(this._novasCategorias);
+  }
+
+  _novasCategorias(categorias) {
+    this.setState({ ...this.state, categorias });
+  }
+
+  _handlerMudancaCategoria(evento) {
+    evento.stopPropagation();
+    this.categoria = evento.target.value;
   }
 
   _handlerMudancaTitulo(evento) {
@@ -21,24 +41,37 @@ class FormularioCadastro extends Component {
   _criarNota(evento) {
     evento.preventDefault();
     evento.stopPropagation();
-    this.props.criarNota(this.titulo, this.texto);
+    this.props.criarNota(this.titulo, this.texto, this.categoria);
   }
 
   render() {
     return (
       <form className="form-cadastro" onSubmit={this._criarNota.bind(this)}>
+        <select
+          onChange={this._handlerMudancaCategoria.bind(this)}
+          className="form-cadastro_input"
+        >
+          <option>Sem Categoria</option>
+
+          {this.state.categorias.map((categoria, index) => {
+            return <option key={index}>{categoria} </option>;
+          })}
+        </select>
+
         <input
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
           onChange={this._handlerMudancaTitulo.bind(this)}
         />
+
         <textarea
           rows={15}
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
           onChange={this._handlerMudancaTexto.bind(this)}
         />
+
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
         </button>
