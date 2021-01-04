8750886c87d788faae46bac7b0fe57904ff9d04f
Marcio Note <mbj_junior@hotmail.com>
2021-01-03 21:17:29 -0300

    Review DadosPessoais

--- oumeajuda/src/component/formularioCadastro/DadosPessoais.jsx
+++ oumeajuda/src/component/formularioCadastro/DadosPessoais.jsx
@@ -1,6 +1,6 @@
 import { Button, FormControlLabel, Switch, TextField } from "@material-ui/core";
 import React, { useContext, useState } from "react";
-import Checkbox from '@material-ui/core/Checkbox';
+import Checkbox from "@material-ui/core/Checkbox";
 import FormGroup from "@material-ui/core/FormGroup";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
@@ -15,14 +15,14 @@ function DadosPessoais({ aoEnviar, getDados }) {
   const validacoes = useContext(ValidacoesCadastro);
   const [erros, validarCampos, possoEnviar] = useErros(validacoes);
   const [checked, setChecked] = React.useState(false);
-  
+
   const handleChangeCheckBox = (event) => {
     setChecked(event.target.checked);
   };
-  
+
   const conectar = async (body) => {
     const usuarioId = getDados().usuarioId;
-    if (usuarioId){
+    if (usuarioId) {
       return await fetch("http://localhost:7000/usuarios/" + usuarioId, {
         method: "PUT",
         headers: {
@@ -34,16 +34,16 @@ function DadosPessoais({ aoEnviar, getDados }) {
         .then((resp) => {
           if (resp.status === 200) {
             return resp.json();
-          } else if (resp.status === 500){
+          } else if (resp.status === 500) {
             const error = new Error(resp.error);
-            alert('Erro de aplicação. Tente novamente mais tarde.');
+            alert("Erro de aplicação. Tente novamente mais tarde.");
             return error;
           }
         })
         .then((json) => {
           return json;
-        })
-     }
+        });
+    }
   };
 
   let alterarUsuario = () => {
@@ -51,9 +51,9 @@ function DadosPessoais({ aoEnviar, getDados }) {
       name: nome,
       lastname: sobrenome,
       type: check ? "M" : "A",
-      phone: celular
-      }
-      
+      phone: celular,
+    };
+
     return conectar(body);
   };
 
@@ -65,100 +65,101 @@ function DadosPessoais({ aoEnviar, getDados }) {
 
   return (
     <>
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        if (possoEnviar()) {
-          alterarUsuario().then(json => {
-            console.log(json)
-            console.log(aoEnviar)
-            aoEnviar(json)
-          });
-        }
-      }}
-    >
-      <TextField
-        value={nome}
-        onChange={(event) => {
-          setNome(event.target.value);
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          if (possoEnviar()) {
+            alterarUsuario().then((json) => {
+              console.log(json);
+              console.log(aoEnviar);
+              aoEnviar(json);
+            });
+          }
         }}
-        onBlur={validarCampos}
-        error={!erros.nome.valido}
-        helperText={erros.nome.texto}
-        id="nome"
-        name="nome"
-        label="Nome"
-        variant="outlined"
-        margin="normal"
-        fullWidth
-      />
-      <TextField
-        value={sobrenome}
-        onChange={(event) => {
-          setSobrenome(event.target.value);
-        }}
-        onBlur={validarCampos}
-        error={!erros.sobrenome.valido}
-        helperText={erros.sobrenome.texto}
-        id="sobrenome"
-        name="sobrenome"
-        label="Sobrenome"
-        variant="outlined"
-        margin="normal"
-        fullWidth
-      />
-      
-      <TextField
-        value={celular}
-        onChange={(event) => {
-          setCelular(event.target.value);
-        }}
-        onBlur={validarCampos}
-        error={!erros.celular.valido}
-        helperText={erros.celular.texto}
-        id="celular"
-        name="celular"
-        label="Celular"
-        variant="outlined"
-        fullWidth
-      /> 
-      <br></br>
-      <br></br>
-      <FormGroup>
-        <Typography component="div">
-          <h3>Selecione uma opção:</h3>
-          <Grid component="label" container alignItems="center" spacing={2}>
-            <Grid item>Aluno</Grid>
-            <Grid item>
-              <AntSwitch
-                checked={check}
-                onChange={handleChangeSelecao}
-                name="selecao"
-              />
-            </Grid>
-            <Grid item>Monitor</Grid>
-          </Grid>
-        </Typography>
-      </FormGroup>
-      
-      <br></br>
-      <br></br>
-      <br></br>
-      <FormControlLabel
-        control={
-        <Checkbox
-          checked={checked}
-          onChange={handleChangeCheckBox}
-          color="primary"
-          inputProps={{ 'aria-label': 'secondary checkbox' }}
-        />}
-        label="Permito que meus dados sejam compartilhados com os monitores."
-      />    
+      >
+        <TextField
+          value={nome}
+          onChange={(event) => {
+            setNome(event.target.value);
+          }}
+          onBlur={validarCampos}
+          error={!erros.nome.valido}
+          helperText={erros.nome.texto}
+          id="nome"
+          name="nome"
+          label="Nome"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+        />
+        <TextField
+          value={sobrenome}
+          onChange={(event) => {
+            setSobrenome(event.target.value);
+          }}
+          onBlur={validarCampos}
+          error={!erros.sobrenome.valido}
+          helperText={erros.sobrenome.texto}
+          id="sobrenome"
+          name="sobrenome"
+          label="Sobrenome"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+        />
 
-      <Button type="submit" variant="contained" color="primary" fullWidth>
-        Cadastrar
-      </Button>
-    </form>
+        <TextField
+          value={celular}
+          onChange={(event) => {
+            setCelular(event.target.value);
+          }}
+          onBlur={validarCampos}
+          error={!erros.celular.valido}
+          helperText={erros.celular.texto}
+          id="celular"
+          name="celular"
+          label="Celular"
+          variant="outlined"
+          fullWidth
+        />
+        <br></br>
+        <br></br>
+        <FormGroup>
+          <Typography component="div">
+            <h3>Selecione uma opção:</h3>
+            <Grid component="label" container alignItems="center" spacing={2}>
+              <Grid item>Aluno</Grid>
+              <Grid item>
+                <AntSwitch
+                  checked={check}
+                  onChange={handleChangeSelecao}
+                  name="selecao"
+                />
+              </Grid>
+              <Grid item>Monitor</Grid>
+            </Grid>
+          </Typography>
+        </FormGroup>
+
+        <br></br>
+        <br></br>
+        <br></br>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={checked}
+              onChange={handleChangeCheckBox}
+              color="primary"
+              inputProps={{ "aria-label": "secondary checkbox" }}
+            />
+          }
+          label="Permito que meus dados sejam compartilhados com os monitores."
+        />
+
+        <Button type="submit" variant="contained" color="primary" fullWidth>
+          Cadastrar
+        </Button>
+      </form>
     </>
   );
 }
@@ -170,7 +171,7 @@ const AntSwitch = withStyles((theme) => ({
     width: 40,
     height: 20,
     padding: 0,
-    display: "flex"
+    display: "flex",
   },
   switchBase: {
     padding: 2,
@@ -181,9 +182,9 @@ const AntSwitch = withStyles((theme) => ({
       "& + $track": {
         opacity: 1,
         backgroundColor: theme.palette.primary.main,
-        borderColor: theme.palette.primary.main
-      }
-    }
+        borderColor: theme.palette.primary.main,
+      },
+    },
   },
   thumb: {
     width: 15,
@@ -194,7 +195,7 @@ const AntSwitch = withStyles((theme) => ({
   track: {
     borderRadius: 16 / 2,
     opacity: 1,
-    backgroundColor: theme.palette.primary.main
+    backgroundColor: theme.palette.primary.main,
   },
-  checked: {}
+  checked: {},
 }))(Switch);
